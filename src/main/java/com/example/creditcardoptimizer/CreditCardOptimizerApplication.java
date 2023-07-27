4903c0e346eb0fb6e828546269a3b5158331845c
Gunjan Dixit <gunjandixit@Gunjans-MacBook-Pro.local>
2023-07-27 14:59:55 +0530

    initial commit

---
+++ src/main/java/com/example/creditcardoptimizer/CreditCardOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditcardoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creditcardoptimizer/CreditCardOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditcardoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
